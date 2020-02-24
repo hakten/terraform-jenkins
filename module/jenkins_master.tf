b48a38d317bfa515a3f1746b867dfdddc9d846e7
Huseyin Akten <Huseyin@Huseyins-MacBook-Pro.local>
2020-02-23 21:54:49 -0600

    yes

--- module/jenkins_master.tf
+++ module/jenkins_master.tf
@@ -1,12 +1,16 @@
 data "aws_ami" "centos" {
   most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 
   filter {
     name   = "name"
-    values = ["CentOS Linux 7 x86_64 HVM EBS ENA 1901_01-b7*"]
+    values = ["CentOS Linux 7 x86_64 HVM EBS 1901_01-b7*"]
   }
-  
-  owners = ["679593333241"]
 }
 
 resource "aws_instance" "jenkins_master" {
